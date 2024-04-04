e73163a7b4aabf08d932f5dfe76d84bac8196645
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-04-04 18:04:33 +0200

    feat: pari dispari exercise completed

--- pari_dispari/js/function.js
+++ pari_dispari/js/function.js
@@ -1,10 +1,25 @@
 /**
  * creare un numero randomico da 1 a 5
- * @param {number} numb scelto dall'utente
  * @returns {number} numero random da 1 a 5
  */
 function randomNumber() {
     const rndNumb = Math.floor(Math.random() * 5) + 1;
     console.log(rndNumb, "numero casuale");
     return rndNumb;
+}
+
+/**
+ * stampare se un numero è pari o dispari
+ * @param {number} evenOddNumb numero da determinare pari o dispari
+ * @returns {string} 
+ */
+function evenOrOddFunc (evenOddNumb) {
+    let evenOddResult = "";
+    if (evenOddNumb % 2 === 0) {
+        evenOddResult = "pari";
+    } else {
+        evenOddResult = "dispari"
+    }
+    console.log("il numero è", evenOddResult);
+    return evenOddResult;
 }
--- pari_dispari/js/script.js
+++ pari_dispari/js/script.js
@@ -1,13 +1,22 @@
 // INPUT
-// userChoice = prompt("scegli pari o dispari"); // string
+userChoice = prompt("scegli pari o dispari"); // string
+console.log(userChoice);
 userNumber = parseInt(prompt("inserisci un numero da 1 a 5")); // number
 console.log(userNumber, "numero utente");
 
 // LOGICA
 // funzione numero casuale
 const random = randomNumber();
-
 // somma numero casuale con numero utente
 const result = random + userNumber;
-console.log(result);
+console.log(result, "somma numeri");
 
+// funzione per stampare pari o dispari in base a somma numeri
+const evenOddTxt = evenOrOddFunc(result);
+
+// condizione vittoria
+if (userChoice === evenOddTxt) {
+    console.log("hai vinto");
+} else {
+    console.log("hai perso");
+}
